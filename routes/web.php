64e5610581940e6ea9cc4b09d984591ad8779b4d
rahmad <rahmadprasetiawan@gmail.com>
2024-04-25 11:56:18 +0700

    pemasangan laravel/ui

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/tes-git', function () {
+    return "Hallo, ini cuma coba pakai git";
+});
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
