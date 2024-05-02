11053fa70ededea77fcd7ec06dad31adf565db39
danil <mdanilakbar.005@gmail.com>
2024-05-02 12:33:47 +0700

    halaman mahasiswa

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JurusanController;
+use App\Http\Controllers\MahasiswaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,4 +31,4 @@ Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('ho
 
 Route::get('/jurusan', [JurusanController::class, 'index']);
 
-Route::get('/mahasiswa', [JurusanController::class, 'index']);
+Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
