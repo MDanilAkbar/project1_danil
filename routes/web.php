3bc3776d42c8f1942eebe1aea70148d3b90ccd7e
danil <mdanilakbar.005@gmail.com>
2024-05-02 09:56:13 +0700

    Penyesuaian tampilan home & login

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 
-Route::get('/tes-git', function () {
-    return "Hallo, ini cuma coba pakai git";
-});
+// Route::get('/tes-git', function () {
+//     return "Hallo, ini cuma coba pakai git";
+// });
 
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
