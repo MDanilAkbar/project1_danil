ff6d62b1b8e3a90ec95e83a4406f1bf995578ac2
danil <mdanilakbar.005@gmail.com>
2024-05-02 12:12:02 +0700

    data jurusan part 2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JurusanController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,3 +27,7 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/jurusan', [JurusanController::class, 'index']);
+
+Route::get('/mahasiswa', [JurusanController::class, 'index']);
